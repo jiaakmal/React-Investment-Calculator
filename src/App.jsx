19c4e48e45e943c228cdfb1dd9d82e9d28e0375e
jiaakmal <160829841+jiaakmal@users.noreply.github.com>
2024-09-08 17:29:05 +0500

    removing bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from "react";
-import Header from "./components/Header"
-import UserInput from "./components/UserInput"
-import Results from "./components/Results"
 
-// Define the App component that renders the Header and UserInput components
+import { useState } from 'react';
+
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
 
 function App() {
-  const [userInput , setUserInput]= useState({
+  const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
-    duration: 10
-   });
-   const isValidDuration = userInput.duration >= 1
- 
+    duration: 10,
+  });
 
-   function handleChange(inputIdentifier , newValue){
-    setUserInput(prevUserInput => {
-        return {...prevUserInput , [inputIdentifier] : newValue}
-    })
+  const inputIsValid = userInput.duration >= 1;
 
-   }
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
 
   return (
     <>
-    <Header/>
-    <UserInput onChange={handleChange} userInput={userInput}/>
-    {isValidDuration ? <Results input={userInput}/> : <p className="center">Enter A Valid Duration</p>}
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
 
-export default App
+export default App;
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,33 +1,43 @@
-import { calculateInvestmentResults , formatter} from '../util/investment.js';
+
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
+
 export default function Results({ input }) {
-  const resultData = calculateInvestmentResults(input);
-  const initialInvestment = resultData[0].valueEndOfYear - resultData[0].interest - resultData[0].annualInvestment
+  const resultsData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
 
   return (
-    <table id='result'>
+    <table id="result">
       <thead>
         <tr>
           <th>Year</th>
-          <th>Interest Earned</th>
           <th>Investment Value</th>
+          <th>Interest (Year)</th>
           <th>Total Interest</th>
           <th>Invested Capital</th>
         </tr>
       </thead>
       <tbody>
-        {resultData.map((yearData) => {
-          const totalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
+        {resultsData.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
           const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
-          return <tr>
-            <td>{yearData.year}</td>
-            <td>{formatter.format(yearData.interest)}</td>
-            <td>{formatter.format(yearData.valueEndOfYear)}</td>
-            <td>{formatter.format(totalInterest)}</td>
-            <td>{formatter.format(totalAmountInvested)}</td>
 
-          </tr>
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
         })}
       </tbody>
     </table>
-      );
+  );
 }
